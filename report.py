b79f430d19d6345f7ebca4320b3a4ee93f34d00e
Mohammed Irfan <95955810+Faux28@users.noreply.github.com>
2022-04-18 13:58:12 +0530

    Update report.py

--- report.py
+++ report.py
@@ -2,9 +2,27 @@ import psycopg2
 from dbconfig import config
 
 
-def product_vendor(id):
+def vendor_report(id):
     conn = None
-    sql="SELECT vendors.vendor_id,vendors.vendor_name FROM vendors inner join vendor_parts on vendor_parts.vendor_id = vendors.vendor_id where vendor_parts.part_id=%s"
+    sql="SELECT vendors.vendor_id,vendors.vendor_name FROM vendors inner join vendor_parts on vendor_parts.vendor_id = vendors.vendor_id where vendor_parts.part_id=%s order by vendor_id"
+    try:
+        params = config()
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+        cur.execute(sql,(id,))
+        rows = cur.fetchall()
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+    return rows
+
+
+def product_report(id):
+    conn = None
+    sql="SELECT * FROM parts where part_id=%s"
     try:
         params = config()
         conn = psycopg2.connect(**params)
